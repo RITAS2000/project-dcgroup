7042682cb2ab8b82e842aaf819dd1bead5ece97c
RITAS <yuliya.lev.2000@gmail.com>
2024-12-13 04:52:43 +0200

    jsimg

--- src/main.js
+++ src/main.js
@@ -8,3 +8,17 @@ modalBtnClose.addEventListener('click', toggleModal);
 function toggleModal() {
   modal.classList.toggle('is-open');
 }
+
+// Отримуємо всі зображення з класом "clickable-image"
+const images = document.querySelectorAll('.clickable-image');
+
+// Отримуємо textarea
+const textarea = document.getElementById('text-area');
+
+// Додаємо обробник подій для кожного зображення
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        // Вставляємо alt текст у textarea
+        textarea.value = image.alt;
+    });
+});
