615a5d7c2e9450b633291865eaa7614b97c5e2a8
RITAS <yuliya.lev.2000@gmail.com>
2024-12-17 00:50:43 +0200

    fixmodaljs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 const modalBtnOpen = document.querySelector('.js-open-modal');
 const modalBtnClose = document.querySelector('.js-close-modal');
- const modalBackdrop = document.querySelector('.mobile-menu-backdrop');
+const modalBackdrop = document.querySelector('.mobile-menu-backdrop'); // Правильна змінна для фону
+const modal = document.querySelector('.js-modal'); // Ось тут ми додали визначення самого модального вікна
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
 
 function toggleModal() {
-  modal.classList.toggle('is-open');
+  modal.classList.toggle('is-open');  // Додаємо/видаляємо клас на модальне вікно
+  modalBackdrop.classList.toggle('is-open'); // Також додаємо/видаляємо клас на фон
 };
 
 const images = document.querySelectorAll('.clickable-image');
